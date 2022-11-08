e68fb7ad2d48d3ccde789ad67666f3c2b412a8ab
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-11-08 11:17:55 -0600

    Added meal object

--- objectOrientationDemo.js
+++ objectOrientationDemo.js
@@ -9,3 +9,10 @@ let fName = 'firstName'
 console.log('Dot notation', person.firstName);
 console.log('Bracket notation', person['firstName']);
 console.log('Bracket notation', person[fName]);
+
+let meal = {
+    appetiter: 'bread',
+    entree: 'ribs',
+    dessert: 'cake',
+    drink: 'sprite'
+}
