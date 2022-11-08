6e949239e8b0e1da93e56e66f12ab4e2fa7dd157
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-11-08 11:16:03 -0600

    Added objectOrientationDemo.js

---
+++ objectOrientationDemo.js
@@ -0,0 +1,11 @@
+let person = {
+    'firstName': 'Joy',
+    'lastName': 'Umoekpo',
+    age: 24
+};
+
+let fName = 'firstName'
+
+console.log('Dot notation', person.firstName);
+console.log('Bracket notation', person['firstName']);
+console.log('Bracket notation', person[fName]);
